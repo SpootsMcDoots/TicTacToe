aa46d819ce7bebeec3cd8fbe37de3d7d783d22f8
Komunela <matkomunela@gmail.com>
2023-09-12 12:05:36 +0800

    Catchup Commit

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -8,6 +10,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Fragment Declaration
+    MenuFragment menuFragment = new MenuFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
     /**
     *   I'm just chucking variables in atm
     */
@@ -21,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void loadMenuFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.frameLayout);
+        if(frag == null) {
+            fm.beginTransaction().add(R.id.frameLayout, menuFragment).commit();
+        }
+        else{
+            fm.beginTransaction().replace(R.id.frameLayout, menuFragment).commit();
+        }
     }
 }
