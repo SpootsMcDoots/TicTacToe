ed7fad28189d6ac05e859db2000c486dd67e96d4
Matthew0yuan <inkayzao@outlook.com>
2023-09-13 22:04:21 +0800

    profile and fragments

--- app/src/main/java/com/example/tictactoe/Profile.java
+++ app/src/main/java/com/example/tictactoe/Profile.java
@@ -1,4 +1,21 @@
 package com.example.tictactoe;
 
 public class Profile {
+    String username;
+    int winCount = 0;
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void win_match(){
+        this.winCount++;
+    }
+
+    public int win_number(){
+        return winCount;
+    }
 }
