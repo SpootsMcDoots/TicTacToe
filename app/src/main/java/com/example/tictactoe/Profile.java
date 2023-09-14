4a222ce79b44e585ffcfe35b1287b6309d0b3745
Matthew0yuan <inkayzao@outlook.com>
2023-09-14 14:51:04 +0800

    changes to profile

--- app/src/main/java/com/example/tictactoe/Profile.java
+++ app/src/main/java/com/example/tictactoe/Profile.java
@@ -3,6 +3,9 @@ package com.example.tictactoe;
 public class Profile {
     String username;
     int winCount = 0;
+    int loseCount = 0;
+    int draw = 0;
+
     public String getUsername(){
         return username;
     }
@@ -11,8 +14,27 @@ public class Profile {
         this.username = username;
     }
 
+    // Call the following methods to update the number of win/lose/draw
     public void win_match(){
         this.winCount++;
+    }
+
+    public void lost_match(){
+        this.loseCount++;
+    }
+
+    public void draw_match(){
+        this.draw++;
+    }
+
+    // Return win/lose/draw percentages for the user,
+    public float[] user_statistic(){
+        float total = draw + loseCount + winCount;
+        float drawPercentage = (float) draw / total;
+        float losePercentage = (float) loseCount / total;
+        float winPercentage = (float) winCount / total;
+//NOTE: return an array in here
+        return new float[]{drawPercentage, losePercentage, winPercentage};
     }
 
     public int win_number(){
