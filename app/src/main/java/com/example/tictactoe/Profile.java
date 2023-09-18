6a9296d520887bff4b81e53cdb147dd42fb5c870
Komunela <matkomunela@gmail.com>
2023-09-18 14:26:35 +0800

    Adding one bracket

--- app/src/main/java/com/example/tictactoe/Profile.java
+++ app/src/main/java/com/example/tictactoe/Profile.java
@@ -11,42 +11,42 @@ public class Profile {
     private int draw = 0;
     private int avatarId = 0;
     private int markerId = 0;
-  
+
     public Profile() {
-      //TODO default constructor & param constructor
+        //TODO default constructor & param constructor
     }
-    
+
     public int getAvatarId() {
-      return avatarId;
+        return avatarId;
     }
 
     public void setAvatarId(int newDrawableId) {
         avatarId = newDrawableId;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username){
+    public void setUsername(String username) {
         this.username = username;
     }
 
     // Call the following methods to update the number of win/lose/draw
-    public void win_match(){
+    public void win_match() {
         this.winCount++;
     }
 
-    public void lost_match(){
+    public void lost_match() {
         this.loseCount++;
     }
 
-    public void draw_match(){
+    public void draw_match() {
         this.draw++;
     }
 
     // Return win/lose/draw percentages for the user,
-    public float[] user_statistic(){
+    public float[] user_statistic() {
         float total = draw + loseCount + winCount;
         float drawPercentage = (float) draw / total;
         float losePercentage = (float) loseCount / total;
@@ -55,6 +55,7 @@ public class Profile {
         return new float[]{drawPercentage, losePercentage, winPercentage};
     }
 
-    public int win_number(){
+    public int win_number() {
         return winCount;
     }
+}
