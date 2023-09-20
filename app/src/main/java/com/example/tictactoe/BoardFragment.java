c54b1094472b23e08a198b17f8ca944e08bdf671
RebeccaM <reassey2000@outlook.com>
2023-09-20 16:01:21 +0800

    Created countdown timer for boardFragment

--- app/src/main/java/com/example/tictactoe/BoardFragment.java
+++ app/src/main/java/com/example/tictactoe/BoardFragment.java
@@ -7,14 +7,19 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class BoardFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -56,6 +61,33 @@ public class BoardFragment extends Fragment {
         AppData mainActivityDVM = new ViewModelProvider(getActivity()).get(AppData.class);
 
         Button back = rootView.findViewById(R.id.backButton);
+        Chronometer timer = rootView.findViewById(R.id.timer);
+        TextView playerIndicator = rootView.findViewById(R.id.tv_current_player);
+
+        //Countdown timer
+        TextView countdown = rootView.findViewById(R.id.countdown);
+        long duration = TimeUnit.MINUTES.toMillis(1);
+
+        new CountDownTimer(duration, 1000) {
+            @Override
+            public void onTick(long ll) {
+                //Converting milliseconds to minutes and seconds
+                //Ref youtube: https://www.youtube.com/watch?v=T_wSEnqGPdo
+                String sDuration = String.format(Locale.ENGLISH, "%02d : %02d"
+                        , TimeUnit.MILLISECONDS.toMinutes(ll), TimeUnit.MILLISECONDS.toSeconds(ll)
+                        - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(ll)));
+
+                //set this to text view
+                countdown.setText(sDuration);
+            }
+
+            //TODO: use this to switch turns automatically
+            @Override
+            public void onFinish() {
+                //TODO: Toast is for small pop up info, use this for win/draw/invalid notifications
+                Toast.makeText(getActivity(), "Turn has ended.", Toast.LENGTH_LONG).show();
+            }
+        }.start();
 
         RecyclerView rv = rootView.findViewById(R.id.boardRecyclerView);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), gameDVM.getBoardSize(), GridLayoutManager.VERTICAL, false);
@@ -69,11 +101,15 @@ public class BoardFragment extends Fragment {
            mainActivityDVM.players.getValue().add(gameDVM.getPlayer1());
            mainActivityDVM.players.getValue().add(gameDVM.getPlayer2());
            Log.d("TAG", Integer.toString(gameDVM.getPlayer1().getProfileID()) + Integer.toString(gameDVM.getPlayer2().getProfileID()));
-       }
-       gameDVM.newGame();
+        }
+        gameDVM.newGame();
+        //Start the count up timer to track total game duration
+        timer.start();
 
-        //TODO
-       //Log.d("TAG", (Arrays.deepToString(board.getBoard())));
+        //TODO: use these in the switch turn methods!
+        //This displays the current players turn.
+        playerIndicator.setText(gameDVM.player1.getValue().getUsername());
+        //TODO: Can probably display the profile pic in an imageview as well
 
        back.setOnClickListener(new View.OnClickListener() {
            @Override
